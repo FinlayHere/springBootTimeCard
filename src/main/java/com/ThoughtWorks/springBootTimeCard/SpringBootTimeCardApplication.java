1c81c2a6c5bcfbf6b88f9bd948c966077911c82a
fanchaokong <finlaykong@126.com>
2020-02-21 20:26:38 +0800

    Initial commit

---
+++ src/main/java/com/ThoughtWorks/springBootTimeCard/SpringBootTimeCardApplication.java
@@ -0,0 +1,13 @@
+package com.ThoughtWorks.springBootTimeCard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTimeCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTimeCardApplication.class, args);
+	}
+
+}
