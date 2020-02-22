a65f831e3521a849861f9baa0785a15b73e9b04d
fanchaokong <finlaykong@126.com>
2020-02-22 19:40:23 +0800

    通过@ExceptionHandler 处理异常信息

--- src/main/java/com/ThoughtWorks/springBootTimeCard/controllers/TimeCardController.java
+++ src/main/java/com/ThoughtWorks/springBootTimeCard/controllers/TimeCardController.java
@@ -6,7 +6,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.ConstraintViolationException;
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -21,5 +24,13 @@ public class TimeCardController {
     @ResponseBody
     public ResponseEntity<String> addTimeCard(@Valid @RequestBody TimeCard timeCard) {
         return ResponseEntity.status(HttpStatus.CREATED).body(service.addTimeCard(timeCard));
+    }
+
+    @ExceptionHandler({javax.validation.ConstraintViolationException.class})
+    @ResponseBody
+    public ResponseEntity<List<String>> timeCardExceptionHandler(ConstraintViolationException e){
+        List<String> res = new ArrayList<>();
+        e.getConstraintViolations().forEach(constraintViolation -> {res.add(constraintViolation.getMessage()+"\n");});
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(res);
     }
 }
